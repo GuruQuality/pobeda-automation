db333623619f7a5a67da075f1860c6f781082320
Katerina <tsvetkova.e.i@mail.ru>
2026-02-11 20:57:02 +0300

    test search-result

--- src/main/java/org/example/PobedaHomePage.java
+++ src/main/java/org/example/PobedaHomePage.java
@@ -14,13 +14,7 @@ public class PobedaHomePage {
     private WebDriver driver;
     private WebDriverWait wait;
 
-    @FindBy(xpath = "//title")
-    WebElement pageTitle;
-
-    @FindBy(xpath = "//*[contains(@class,'dp-1a6miki-root-root-root')]/*[1]")
-    WebElement logo;
-
-    @FindBy(xpath = "//*[contains(@class,'dp-1x07rlv-lottie')]/*[1]")
+    @FindBy(css = "[aria-label='«Авиакомпания «Победа», Группа «Аэрофлот»']")
     WebElement logo2;
 
     @FindBy(xpath = "//a[@href='/information']")
@@ -38,6 +32,11 @@ public class PobedaHomePage {
     public String url = "https://pobeda.aero";
 
     private SoftAssertions softly;
+
+    //Константы Заголовков
+    private static final String EXPECTED_FLIGHT_PREP = "Подготовка к полёту";
+    private static final String EXPECTED_USEFUL_INFO = "Полезная информация";
+    private static final String EXPECTED_ABOUT_COMPANY = "О компании";
 
     // Конструктор по умолчанию (без параметров)
     public PobedaHomePage() {
@@ -72,11 +71,6 @@ public class PobedaHomePage {
 
     // Проверка отображения логотипа
     public void isLogoDisplayed() {
-        // 4. Получить внутренний HTML
-//        String innerHtml = logo2.getAttribute("innerHTML");
-//        String outerHtml = logo2.getAttribute("outerHTML");
-//        System.out.println("Внутренний HTML: " + innerHtml);
-//        System.out.println("Наружний HTML: " + outerHtml);
         try {
             wait.until(ExpectedConditions.visibilityOf(logo2));
             softly.assertThat(logo2.isDisplayed())
@@ -108,14 +102,14 @@ public class PobedaHomePage {
 
     //Проверка заголовков в Popup "Информация"
     public void verifyPopupHeaders() {
-        softly.assertThat(flightPreparation.isDisplayed())
-                .as("Заголовок 'Подготовка к полету' не отображается")
-                .isTrue();
-        softly.assertThat(usefulInfo.isDisplayed())
-                .as("Заголовок 'Полезная информация' не отображается")
-                .isTrue();
-        softly.assertThat(aboutCompany.isDisplayed())
-                .as("Заголовок 'О компании' не отображается")
-                .isTrue();
+        softly.assertThat(flightPreparation.getText())
+                .as("Проверка заголовка '%s'", EXPECTED_FLIGHT_PREP)
+                .isEqualTo(EXPECTED_FLIGHT_PREP);
+        softly.assertThat(usefulInfo.getText())
+                .as("Проверка заголовка '%s'", EXPECTED_USEFUL_INFO)
+                .isEqualTo(EXPECTED_USEFUL_INFO);
+        softly.assertThat(aboutCompany.getText())
+                .as("Проверка заголовка '%s'", EXPECTED_ABOUT_COMPANY)
+                .isEqualTo(EXPECTED_ABOUT_COMPANY);
     }
 }
---
+++ src/main/java/org/example/Pobeda_testEx3.java
@@ -0,0 +1,93 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.concurrent.TimeUnit;
+
+
+import java.lang.module.Configuration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+/*
+Задание №3. Page Object. Результаты поиска
+1. Перейти на сайт pobeda.aero.
+2. Убедиться, что сайт открылся:
+а) текст заголовка страницы: Авиакомпания «Победа» - купить билеты на самолёт дешево онлайн, прямые и трансферные рейсы;
+б) на странице есть логотип Победы.
+3. Проскроллить страницу чуть ниже и кликнуть на пункт «Управление бронированием».
+4. Убедиться, что открылась необходимая страница:
+а) есть поле «Номер заказа или билета»;
+б) есть поле «Фамилия клиента»;
+в) есть кнопка «Поиск».
+5. Ввести в поля ввода данные:
+номер заказа – XXXXXX, фамилия – Qwerty
+и нажать кнопку «Поиск».
+6. Убедиться, что в новой вкладке на экране отображается текст ошибки «Заказ с указанными параметрами не найден».
+ */
+
+public class Pobeda_testEx3 {
+    WebDriver driver;
+    SoftAssertions softly = new SoftAssertions();
+
+
+    @Before
+    public void openDriver() {
+        // 1. Настраиваем драйвер автоматически
+        WebDriverManager.chromedriver().setup();
+
+        // 2. Создаем опции
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("excludeSwitches", List.of("enable-automation"))//Убирает "Chrome is being controlled..
+                .setExperimentalOption("useAutomationExtension", false)//Отключает расширение автоматизации
+                .addArguments("--disable-blink-features=AutomationControlled")//Прячет автоматизацию от JavaScript
+                .addArguments("--disable-infobars")
+                .addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+                .addArguments("--start-maximized")
+                .addArguments("--disable-notifications")//Отключает всплывающие попапы
+                .addArguments("--remote-allow-origins=*");//Разрешает удаленные подключения к драйверу
+
+        // 3. Создаем драйвер
+        driver = new ChromeDriver(options);
+        //driver.get("https://www.pobeda.aero/");
+        driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testTitlePage() throws InterruptedException {
+        //SoftAssertions softly = new SoftAssertions();
+        // 1. Перейти на сайт
+        PobedaHomePage pobedaPage = new PobedaHomePage(driver, softly);
+        pobedaPage.open();
+        pobedaPage.verifyURL();//Проверка, что нужный нам сайт открылся
+
+        pobedaPage.verifyTitle("Авиакомпания «Победа» - купить билеты на самолёт дешево онлайн, прямые и трансферные рейсы");
+        pobedaPage.isLogoDisplayed();
+    }
+
+    @Test
+    public void testISearchResults() throws InterruptedException {
+        // 1. Перейти на сайт
+        BookingManagementPage pobedaPage = new BookingManagementPage(driver, softly);
+        pobedaPage.open();
+        Thread.sleep(1000);
+        pobedaPage.scrollAndClickBookingManagement();// Скролл и клик на пункт «Управление бронированием»
+        Thread.sleep(1000);
+        pobedaPage.verifyURL();
+        pobedaPage.BookingManagementIsDisplayed();// Проверка, что открылась нужная страница
+        pobedaPage.fillSearchFormAndSubmit();// Заполнение формы и ее отправление
+        pobedaPage.errorMessageDisplayed("Заказ с указанными параметрами не найден");// Проверка отображения ошибки
+    }
+
+    @After
+    public void closeDriverAndGetSoftlyAssert() {
+        driver.quit();
+        // ВСЕ проверки будут выполнены, даже если первые упали
+        softly.assertAll(); // Здесь бросится исключение со ВСЕМИ ошибками
+    }
+}
